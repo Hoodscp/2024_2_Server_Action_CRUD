374b2cb2a9c858840eee8115f873f44e7be25ba6
HoodScp <godpd440@naver.com>
2024-11-19 11:52:39 +0900

    웹이추

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         className="text-sky-100 text-4xl font-extrabold hover:text-sky-400"
         href="/"
       >
-        E_plus
+        웹페이지이름추천좀
       </Link>
       <Link
         className="bg-sky-400 text-lg font-bold px-4 py-2 rounded-md hover:bg-sky-600"
